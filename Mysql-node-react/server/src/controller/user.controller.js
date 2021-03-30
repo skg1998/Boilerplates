66e52da6c463ef874b1d6ae3f92686ae7e68dbd0
skg1998 <sahilkg1998@gmail.com>
2021-03-30 14:33:09 +0530

    init commit

---
+++ Mysql-node-react/server/src/controller/user.controller.js
@@ -0,0 +1,51 @@
+const UserModel = require('../modal');
+
+const User = {}
+
+User.signup = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.create({
+            email: email,
+            password: password,
+        });
+        const token = authService().issue({ id: user.id });
+        return res.status(200).json({ token, user });
+    } catch (err) {
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
+}
+
+User.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ where: { email } });
+        if (user.validPassword(String(password))) {
+            const token = authService().issue({ id: user.id });
+            return res.status(200).json({ token, user });
+        }
+        return res.status(401).json({ msg: 'Unauthorized' });
+    } catch (err) {
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
+}
+
+User.updateUserById = async (req, res) => {
+
+}
+
+User.deleteUserById = async (req, res) => {
+
+}
+
+User.getAll = async (req, res) => {
+    try {
+        const users = await UserModel.findAll();
+        return res.status(200).json({ users });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
+}
+
+module.exports = User;
